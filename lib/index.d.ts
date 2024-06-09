42ff60dcebc5abc36b35ebd45dd7dfd701ab6e81
Steven <106362981+stevenleep@users.noreply.github.com>
2024-06-09 08:21:23 +0800

    release(@1.1.2): update api name

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,46 +1,15 @@
 type RequestMethods = "get" | "post" | "put" | "delete" | "patch" | "head" | "options" | "trace" | "connect";
 type SupportedRequestMethods = RequestMethods | Uppercase<RequestMethods> | Capitalize<RequestMethods> | Uncapitalize<RequestMethods>;
 type ServiceConstructorOptions<Namespaces extends Object = Record<string, string>> = {
-    /**
-     * 默认请求方法
-     */
     defaultMethod?: SupportedRequestMethods;
-    /**
-     * Namespaces 命名空间
-     * @example
-     * ```json
-     * {
-     *    "default": "/api/v1",
-     *    "v2": "/api/v2",
-     *    "open": "/open"
-     * }
-     * ```
-     * @type {Record<string, string> ｜ string}
-     */
     namespaces?: string | (Namespaces & {
         default: string;
     });
 };
 type ServiceConfig = {
-    /**
-     * 请求方法
-     */
     method?: SupportedRequestMethods;
-    /**
-     * 调用名称
-     */
-    /**
-     * 请求路径
-     */
     path?: string;
-    /**
-     * 使用的命名空间，默认使用 `default` 命名空间
-     * 如果设置为 string 类型，则使用 `namespaces` 中的对应路径
-     */
     namespace?: string;
-    /**
-     * 使用自定义的请求库实例
-     */
     instance?: any;
 };
 type DefaultServicesConfigMaps = Record<string, ServiceConfig | string>;
@@ -63,9 +32,6 @@ declare class LooseService {
 
 declare function getRequestInstance(rootInstance: any, options: ServiceConfig): any;
 declare function getNamespacePath(namespaces: string | Record<string, string>, namespaceName: string): string;
-/**
- * 创建请求函数
- */
 declare function createRequestFunction(requestInstance: any, serviceConfig: Omit<ServiceConfig, "method"> & {
     method: SupportedRequestMethods;
 }): <Params extends any[] = any[], ResultType = any>(...args: Params) => Promise<ResultType>;
